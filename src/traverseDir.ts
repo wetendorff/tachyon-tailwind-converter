d870738578a7fcad91ff8cc06ed306fbd3c43f63
Lars Wetendorff Nielsen <lars@hejdoktor.dk>
2023-10-17 17:27:31 +0200

    feat: remove console.log and unused code

--- src/traverseDir.ts
+++ src/traverseDir.ts
@@ -14,7 +14,6 @@ export function traverseDirectory(
 
     // If file is a directory that is in the ingoreDirectories list, then ignore it and return
     if (fileStat.isDirectory() && config.ignoreDirectories.includes(file)) {
-      console.log('Ignoring directory: ', filePath)
       return
     }
 
@@ -27,7 +26,6 @@ export function traverseDirectory(
     // We got a file, ignore it if it not of the correct file extension
     const fileExtension = path.extname(filePath)
     if (!config.fileExtensions.includes(fileExtension)) {
-      console.log('Ignoring file: ', filePath)
       return
     }
 
@@ -35,64 +33,3 @@ export function traverseDirectory(
     callback(filePath)
   })
 }
-
-// function traverseDirectory(dir, ignoreDirectories, fileExtensions, wordMap) {
-//   const dirName = path.basename(dir);
-
-//   if (ignoreDirectories.includes(dirName)) {
-//     console.log("Ignoring directory: ", dirName);
-//     return;
-//   }
-
-//   // Read all files in the directory
-//   const files = getFileSync(dir);
-
-//   // Filter out files that are not of the correct file extension
-//   const filteredFiles = files.filter((file) => {
-//     const fileExtension = path.extname(file);
-//     return fileExtensions.includes(fileExtension);
-//   });
-
-//   // Read the content of each file
-
-//   const result = new Map();
-//   filteredFiles.forEach((file) => {
-//     // Get the full path of the file
-//     const filePath = path.join(dir, file);
-
-//     // Check if the file is a directory and if so, then traverse the directory
-//     if (fs.statSync(filePath).isDirectory()) {
-//       traverseDirectory(filePath, ignoreDirectories, fileExtensions, wordMap);
-//       return;
-//     }
-
-//     // Read the file information
-//     const file = Bun.file(filePath);
-
-//     // Read the file content as text
-//     const text = file.text();
-//     const foundWords = parseText(text, wordMap);
-
-//     // Add the found words to the result
-//     foundWords.forEach((count, word) => {
-//       if (!result.has(word)) {
-//         result.set(word, {
-//           word,
-//           count,
-//           files: [filePath],
-//         });
-//         return;
-//       }
-
-//       // If the word already exists in the result, then add the count to the existing count
-//       const currentValue = result.get(word);
-//       const currentCount = currentValue.count;
-//       result.set(word, {
-//         ...currentValue,
-//         count: currentCount + 1,
-//       });
-//     });
-//   });
-
-//   return result;
-// }
