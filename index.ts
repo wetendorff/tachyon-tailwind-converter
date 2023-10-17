2f65b838abea8949139cfe5854bf91d03c7e2997
Lars Wetendorff Nielsen <lars@hejdoktor.dk>
2023-10-17 17:26:17 +0200

    feat: update verbose output

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import { findAllUsedTachyonClasses, parseTachyonCSSFile } from './src/parse'
 import config from './config'
 import db from './src/db'
 import { saveMappingAsJson } from './src/mapping'
+import { findAllUsedTachyonClasses, parseTachyonCSSFile } from './src/parse'
 
 type Command = 'reset' | 'backup' | 'parse'
 
@@ -10,25 +10,28 @@ let commands: Record<Command, () => void | Promise<void>> = {
     console.log('Reset Database')
     db.reset()
   },
+
   backup() {
     console.log('Backup mapping to mapping.json')
     saveMappingAsJson()
   },
+
   async parse() {
     console.log(
       'Parsing Tachyon CSS file and finding all used Tachyon classes in source directory',
     )
-    console.time('time')
     db.init()
     await parseTachyonCSSFile(config.tachyonFile)
     findAllUsedTachyonClasses()
-    console.timeEnd('time')
   },
 }
 
 const command = process.argv[2] as Command
 if (commands[command]) {
-  commands[command]()
+  console.time()
+  await commands[command]()
+  console.timeEnd()
+  console.log('Done!')
 } else {
   console.log(
     `The command '${command}' does not exist. Available commands: reset, backup, show`,
