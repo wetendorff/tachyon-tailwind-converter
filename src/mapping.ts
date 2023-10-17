71b44c4dffdcf0268e6e22da85ef9db74976c2a8
Lars Wetendorff Nielsen <lars@hejdoktor.dk>
2023-10-17 17:29:41 +0200

    refactor: change output format of mapping

--- src/mapping.ts
+++ src/mapping.ts
@@ -4,10 +4,10 @@ export function saveMappingAsJson() {
   const file = Bun.file('mapping.json')
   const writer = file.writer()
 
-  writer.write('[\n')
+  writer.write('{\n')
   db.getAllMappedTachyonClasses().forEach((tachyon) => {
-    writer.write(`  { "${tachyon.tachyon}": "${tachyon.tailwind}" },\n`)
+    writer.write(`  "${tachyon.tachyon}": "${tachyon.tailwind}",\n`)
   })
-  writer.write(']\n')
+  writer.write('}\n')
   writer.end()
 }
