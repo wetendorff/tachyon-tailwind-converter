b98f8c4f606e9fd4a51ec93534b93b5d9e8d8d69
Lars Wetendorff Nielsen <lars@hejdoktor.dk>
2023-10-26 09:53:03 +0200

    docs: clean up and document index file

--- index.ts
+++ index.ts
@@ -11,9 +11,10 @@ import { question } from './src/strings'
 
 type Command = 'reset' | 'backup' | 'restore' | 'parse' | 'replace' | 'debug'
 
-let commands: Record<Command, () => void | Promise<void>> = {
+const commands: Record<Command, () => void | Promise<void>> = {
   /**
    * Resets the database after prompting the user for confirmation.
+   * @returns {Promise<void>} A Promise that resolves when the mapping has been restored.
    */
   async reset() {
     const reset = await question(
@@ -28,16 +29,27 @@ let commands: Record<Command, () => void | Promise<void>> = {
     db.reset()
   },
 
+  /**
+   * Creates a backup of the current mapping to the specified file.
+   */
   backup() {
     console.log(`Backup mapping to: ${config.backupFile}`)
     backupToFile(config.backupFile)
   },
 
+  /**
+   * Restores the mapping from a backup file.
+   * @returns {Promise<void>} A Promise that resolves when the mapping has been restored.
+   */
   async restore() {
     console.log(`Restore mapping from: ${config.backupFile}`)
     await restoreFromFile(config.backupFile)
   },
 
+  /**
+   * Parses the Tachyon CSS file and finds all used Tachyon classes in the source directory.
+   * @returns {Promise<void>} A Promise that resolves when the mapping has been restored.
+   */
   async parse() {
     console.log(
       'Parsing Tachyon CSS file and finding all used Tachyon classes in source directory',
@@ -47,7 +59,11 @@ let commands: Record<Command, () => void | Promise<void>> = {
     await findAllUsedTachyonClasses()
   },
 
-  async replace(): Promise<void> {
+  /**
+   * Replaces Tachyon classes with Tailwind classes.
+   * @returns {Promise<void>} A Promise that resolves when the replacement is complete.
+   */
+  async replace() {
     console.log('Replace Tachyon classes with Tailwind classes')
     const notMapped = db.getNotMappedTailwindClasses()
     if (notMapped.length > 0) {
@@ -66,6 +82,9 @@ let commands: Record<Command, () => void | Promise<void>> = {
   },
 }
 
+/**
+ * The command to be executed, e.g. 'reset', 'backup', 'restore', 'parse', 'replace'
+ */
 const command = process.argv[2] as Command
 if (commands[command]) {
   console.time('time')
